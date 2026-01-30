d9c9d180c2d5fbb0cb6096c6ea3ed8d4275d6ae0
RUDRAKSH KAUSHIK <ruuuview@gmail.com>
2026-01-30 18:34:38 +0530

    Ensure rogue duplicates are also empty

--- ios/WidgetHelper.m
+++ ios/WidgetHelper.m
@@ -1,17 +1,1 @@
-
-
-
-@interface RCT_EXTERN_MODULE(WidgetHelper, NSObject)
-
-
-
-RCT_EXTERN_METHOD(reloadWidget:(NSString *)jsonString
-
-                  resolver:(RCTPromiseResolveBlock)resolver
-
-                  rejecter:(RCTPromiseRejectBlock)rejecter)
-
-
-
-@end
-
+// File deprecated

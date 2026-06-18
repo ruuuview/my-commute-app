0898e0029d367798f866a2a35618d8c3d9302a36
RUDRAKSH KAUSHIK <ruuuview@gmail.com>
2026-06-18 23:53:24 +0530

    fix: wrap WidgetModule in platform-specific checks and add fallback import for RCTBridgeModule

--- ios/MyCommute/WidgetModule.m
+++ ios/MyCommute/WidgetModule.m
@@ -1,6 +1,16 @@
+#import <TargetConditionals.h>
+
+#if TARGET_OS_IPHONE
+
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "../../node_modules/react-native/React/Base/RCTBridgeModule.h"
+#endif
 
 @interface RCT_EXTERN_MODULE(WidgetModule, NSObject)
   RCT_EXTERN_METHOD(reloadWidget:(NSString *)jsonString)
   RCT_EXTERN_METHOD(saveWidgetStatusCache:(NSString *)jsonString)
 @end
+
+#endif

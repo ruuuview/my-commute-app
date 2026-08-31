aa134ad315caf171343448239e848ed7e05af094
ruuuview <rudra00046@gmail.com>
2026-08-31 14:31:08 +0530

    chore: remove deprecated journeyPlanner file

--- app/journeyPlanner.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
-
-export default function JourneyPlannerScreen() {
-  // Redirect to main tabs index route
-  return <Redirect href="/(tabs)" />;
-}
